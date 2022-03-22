3fde37b3d4e84f09e6a8807d0e645481241c269c
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-22 16:56:05 -0300

    refatorando tags

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,7 +1,7 @@
 import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
-import classNames from 'classnames';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
   const { id } = useParams();
@@ -26,23 +26,7 @@ export default function Prato() {
           <p className={styles.conteudo__descriçao}>
             {prato.description}
           </p>
-          <div className={styles.tag}>
-            <div className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-            })}>
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porçao}>
-              {prato.size}g
-            </div>
-            <div className={styles.tags__qtdpessoas}>
-              serve {prato.serving} pessoa {prato.serving === 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>
-              R${prato.price.toFixed(2)}
-            </div>
-          </div>
+          <TagsPrato {...prato}/>
         </div>
       </section>
     </>
